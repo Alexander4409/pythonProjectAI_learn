f707fc6523791676c8add97ec81ce77bf23496f7
Alexander <aleksandr.tailakov@mail.ru>
2026-09-17 18:27:03 +0700

    add lesson_1 for NN-72

---
+++ ИИ_72_кем/Lesson_1.py
@@ -0,0 +1,13 @@
+#пишем переменные осмысленно
+# devochky
+# malchiky - так не делаем!!!
+# используем для переменных анлгийские слова
+# lst_1 = [1,3,2,"hi"]
+#low_snake_case
+#классы и их имена - HiCamelCase
+# def test_memory():
+#     num = 10
+#     print(id(num))
+#
+# test_memory()
+
