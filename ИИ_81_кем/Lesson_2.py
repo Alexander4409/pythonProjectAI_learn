ac2cfd00034027b047eb65502c933d39de1897b8
SayGG <sayginaego.say@gmail.com>
2026-09-23 09:10:50 +0700

    Added enumerate and range

--- ИИ_81_кем/Lesson_2.py
+++ ИИ_81_кем/Lesson_2.py
@@ -9,4 +9,9 @@ for obj in tumb:
 
 print(iter(tumb))
 #<tuple_iterator object at 0x0000025DB11D1C00>
-
+print(iter(range(10)))
+#<range_iterator object at 0x000001F9E56D02D0>
+print(iter(enumerate(tumb)))
+#<enumerate object at 0x0000027DF5F2E340>
+print(iter(zip(tumb)))
+#<zip object at 0x000001D0FB722D40>
