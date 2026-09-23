46d29863527f112409365ec189b2621bf9001440
Alexander <aleksandr.tailakov@mail.ru>
2026-09-23 09:07:56 +0700

    add lesson 2 AI

--- ИИ_81_кем/Lesson_2.py
+++ ИИ_81_кем/Lesson_2.py
@@ -1,1 +1,12 @@
 #Списки и их методы - https://metanit.com/python/tutorial/3.1.php
+#иитератор
+tumb = ("apple", "socks", "pen")
+# tumb.append("pencil")
+# print(tumb)
+# перебор посредством цикла for
+for obj in tumb:
+    print(obj)
+
+print(iter(tumb))
+#<tuple_iterator object at 0x0000025DB11D1C00>
+
