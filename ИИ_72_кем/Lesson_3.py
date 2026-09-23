74c7fccb29b5ca479d5df3fd91a11b0be5333cef
Alexander <aleksandr.tailakov@mail.ru>
2026-09-23 16:54:37 +0700

    update lesson 2, 3 AI

--- ИИ_72_кем/Lesson_3.py
+++ ИИ_72_кем/Lesson_3.py
@@ -3,3 +3,34 @@
 # 1 Мегабайт (Мбайт) = 1024 Килобайта = 𝟐^𝟐𝟎 байтов
 # 1 Гигабайт (Гбайт) = 1024 Мегабайта = 𝟐^𝟑𝟎 байтов
 # 1 Терабайт (Тбайт) = 1024 Гигабайта = 𝟐^𝟒𝟎 байтов
+from random import choice
+
+
+def convert_bytes():
+    try:
+        bytes_count = float(input("enter bytes count: "))
+    except ValueError:
+        print("Error enter correct valid")
+        return
+
+    user_choice = input(f'please enter chosen value: \n'
+                            f'1 - Kb\n'
+                            f'2 - mb\n'
+                            f'3 - GB\n'
+                            f'4 - TB')
+
+    units = {
+        "1": (1024,"kb"),
+        "2": (1024**2,"mb"),
+        "3": (1024**3,"gb"),
+        "4": (1024**4,"tb"),
+    }
+
+    if user_choice in units:
+        divider , unit_name = units[user_choice]
+        res = bytes_count / divider
+        print(f'result - {bytes_count}, bytes - {round(res, 4)}, {unit_name}')
+    else:
+        print("Error, check your data")
+
+convert_bytes()
