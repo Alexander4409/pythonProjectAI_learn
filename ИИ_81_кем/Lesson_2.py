69ffc76594a623b55f7106122445bbebc4f62482
Alexander <aleksandr.tailakov@mail.ru>
2026-09-23 09:55:12 +0700

    add lesson 2 AI

--- ИИ_81_кем/Lesson_2.py
+++ ИИ_81_кем/Lesson_2.py
@@ -1,12 +1,70 @@
 #Списки и их методы - https://metanit.com/python/tutorial/3.1.php
 #иитератор
-tumb = ("apple", "socks", "pen")
+# tumb = ("apple", "socks", "pen")
 # tumb.append("pencil")
 # print(tumb)
 # перебор посредством цикла for
-for obj in tumb:
-    print(obj)
+# for obj in tumb:
+#     print(obj)
+#
+# print(iter(tumb))
+#инкапсулированнная инструкция перебора объекта  - <tuple_iterator object at 0x0000025DB11D1C00>
 
-print(iter(tumb))
-#<tuple_iterator object at 0x0000025DB11D1C00>
+# tumb = ["apple", "socks", "pen"]
+#
+# it = iter(tumb)
+#
+# try:
+#     while True:
+#         next_value = next(it)
+#         print(f"value {next_value}")
+# except StopIteration:
+#     print("Iteration stop")
+# print("program complete")
+
+#класс тумбочка с ящиками
+class Tumb:
+    "класс тумбочка"
+    def __init__(self):
+        self.boxes = {
+            1:[],
+            2:[],
+            3:[]
+        }
+
+    def add_to_box(self,obj,box_num):
+        if box_num not in {1,2,3}:
+            print("Enter a valid value!")
+        else:
+            self.boxes[box_num].append(obj)
+
+    def remove_from_box(self,box_num):
+        if box_num not in {1, 2, 3}:
+            print("Enter a valid value!")
+        else:
+            return self.boxes[box_num].pop()
+
+    def __str__(self):
+        boxes_items = self.boxes[1] + self.boxes[2] + self.boxes[3]
+        return ", ".join(boxes_items)
+
+
+
+
+tumb_1 = Tumb()
+tumb_1.add_to_box("ножницы",1 )
+tumb_1.add_to_box("карандаш",2 )
+tumb_1.add_to_box("яблоко",3 )
+tumb_1.add_to_box("книга",1 )
+#
+# print(tumb_1)
+
+# придумать правило итерации у тумбочки
+
+# так не делаем !
+my_ugly_list = [[{},[]],{},"",tumb_1]
+
+for some_collection in my_ugly_list:
+    for el in some_collection:
+        print(el)
 
