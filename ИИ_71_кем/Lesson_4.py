22291fc62b14fc6708424d89d9b463734dab3a86
vaneksisik-ops <vaneksisik@gmail.com>
2026-09-23 11:22:45 +0700

    Refactor password memory size calculation

--- ИИ_71_кем/Lesson_4.py
+++ ИИ_71_кем/Lesson_4.py
@@ -1,23 +1,13 @@
-# программа для определения объема памяти пользовательского пароля
-# password = input("Enter user password")
-
-# password_bytes = password.encode("utf-8")
-#
-# size_in_bytes = len(password_bytes)
-# size_in_bits = size_in_bytes * 8
-# size_in_kilobytes  = size_in_bytes / 1024
-#
-# print(f"results :\n"
-#       f"User passcode - {password}\n"
-#       f"symbol count - {len(password)} \n"
-#       f"kilobytes - {size_in_kilobytes}
-
-# реальный объем памяти
 import sys
-
-
-password = input("Enter user password")
-
-size_in_bytes = sys.getsizeof(password)
-print(len(password))
-print(size_in_bytes)
+def password_question():
+    password = input("ведите пароль")
+    global byts
+    byts = sys.getsizeof(password)
+def print_size():
+    print(f"kb - {byts / 8 / 1024}\n"
+        f"mb - {byts / 8 / 1024 / 1024}\n"
+        f"gb - {byts / 8 / 1024 / 1024 / 1024}\n"
+        f"tb - {byts / 8 / 1024 / 1024 / 1024 / 1024}\n")
+while True:
+    password_question()
+    print_size()
