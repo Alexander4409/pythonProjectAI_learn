e88aef6f46bb6b11da1969ccad3d5526f6f3fece
vbif2703 <Mishailivas@yandex.ru>
2026-09-24 09:46:15 +0700

    Update Lesson_5.py

--- ИИ_71_кем/Lesson_5.py
+++ ИИ_71_кем/Lesson_5.py
@@ -1,4 +1,15 @@
-# написать программу для расчета объема памяти необходимого для хранения паролей пользователей
-# пользователь вводит мощность алфавита
-# вводит длину паролей
-# вводит количество пользователей
+def object():
+    sim = int(input())
+    pas = int(input())
+    user = int(input())
+
+    bits_on_sym = 0
+    while(1 << bits_on_sym) < sim:
+        bits_on_sym += 1
+
+    pass_len = bits_on_sym * pas
+    result = pass_len * user
+
+    print(f'bits: {result}')
+
+object()
