1d948f48599784d5130a42e0c16eed3c7e488901
trifuzanton330-collab <trifuzanton330@gmail.com>
2026-09-24 09:20:26 +0700

    Update Lesson_5.py
    01011000101011011010110101010101011101010

--- ИИ_71_кем/Lesson_5.py
+++ ИИ_71_кем/Lesson_5.py
@@ -1,4 +1,23 @@
-# написать программу для расчета объема памяти необходимого для хранения паролей пользователей
-# пользователь вводит мощность алфавита
-# вводит длину паролей
-# вводит количество пользователей
+a = int(input("Введите количество паролей"))
+b = int(input("Длина одного пароля"))
+c = int(input("Информационный вес одного символ"))
+bit = b * c
+byts = bit / 8
+ff = byts * a
+user_choise = int(input(f"Choose\n"
+                        f"1.KB\n"
+                        f"2.MB\n"
+                        f"3.GB\n"
+                        f"4.TB\n"
+                        f"Ваш выбор: "))
+units = {1: (1024, "KB"),
+         2: (1024**2, "MB"),
+         3: (1024**3, "GB"),
+         4: (1024**4, "TB")}
+if user_choise in units:
+    divider, unit_name = units[user_choise]
+    res = ff / divider
+    print(f"Результат в {unit_name}: {res}")
+else:
+    print("Error")
+print(f"Количество байт: {ff}")
